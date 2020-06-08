f6394e9a26cf8ae5c63b544af4a6a2fc5e752fc1
Max Niederman <maxniederman@gmail.com>
2020-06-08 14:38:16 -0700

    Added commands to change permissions

--- extensions/authconfig.py
+++ extensions/authconfig.py
@@ -30,14 +30,47 @@ class AuthConfig(commands.Cog):
         """Set whitelisted roles for Member
 
         Args:
-            member: Member to set whitelist roles
-            roles: Roles to whitelist
+            member: Member to set whitelist roles.
+            roles: Roles to whitelist.
         """
         whitelist = ctx.bot.auth['whitelist']
-        for role in roles:
-            whitelist[str(member.id)] = [role.id for role in roles]
+        whitelist[str(member.id)] = [role.id for role in roles]
         
         await ctx.send(f'Set whitelists for {member.display_name}.')
+    
+    @commands.command()
+    async def SetCommandPerms(self, ctx, command_name: str, *roles: discord.Role):
+        """Set roles which can run a command
+
+        Args:
+            command: Command to set permissions for.
+            roles: Roles to grant permission. If you specify none, any member can run the command.
+        """
+        command = ctx.bot.get_command(command_name)
+        if command:
+            permissions = ctx.bot.auth['permissions']
+            permissions[command.cog_name][command.name] = [role.id for role in roles]
+            
+            await ctx.send(f'Set permissions for command {command.name}.')
+        else:
+            await ctx.send(f'No such command {command_name}.')
+    
+    @commands.command()
+    async def SetCogPerms(self, ctx, cog_name: str, *roles: discord.Role):
+        """Set roles which can by default run commands for a cog
+
+        Args:
+            cog: Cog to set permissions for.
+            roles: Roles to grant permission. If you specify none, any member can run the command.
+        """
+        cog = ctx.bot.get_cog(cog_name)
+        if cog:
+            permissions = ctx.bot.auth['permissions']
+            permissions[cog.name]['__default__'] = [role.id for role in roles]
+            
+            await ctx.send(f'Set permissions for cog {cog.name}.')
+        else:
+            await ctx.send(f'No such cog {cog_name}.')
     
 def setup(bot):
     bot.add_cog(AuthConfig(bot))
